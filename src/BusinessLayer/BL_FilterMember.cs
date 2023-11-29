3024a6c0b7cf4fb0002cdd4ddc2874adf92d3a2b
Büşra ipek <105125385+busraipek@users.noreply.github.com>
2023-11-30 00:54:16 +0300

    Add files via upload

--- src/BusinessLayer/BL_FilterMember.cs
+++ src/BusinessLayer/BL_FilterMember.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.OleDb;
 using System.Linq;
 using System.Text;
@@ -9,16 +8,30 @@ using DataAccessLayer;
 
 namespace BusinessLayer
 {
-    public class BL_MemberList
+    public class BL_FilterMember
     {
-        public void GetMember(string[,] membersArray)
+        public void FilterMember(string[,] membersArray, string filter, string text)
         {
             OleDbConnection connection = new OleDbConnection("Provider=Microsoft.ACE.Oledb.12.0;Data Source=C:\\Users\\90505\\Desktop\\Database4.accdb");
             {
+                string query;
                 try
                 {
                     connection.Open();
-                    string query = "SELECT ad, soyad, cinsiyet, dogum_tarihi, kimlik_no, kan_grubu, uyelik_durumu, e_posta, sehir FROM uye";
+                    if (filter == "ad")
+                        query = "SELECT ad, soyad, cinsiyet, dogum_tarihi, kimlik_no, kan_grubu, uyelik_durumu, e_posta, sehir FROM uye where ad ='" + text + "'";
+                    else if (filter == "soyad")
+                        query = "SELECT ad, soyad, cinsiyet, dogum_tarihi, kimlik_no, kan_grubu, uyelik_durumu, e_posta, sehir FROM uye where soyad ='" + text + "'";
+                    else if (filter == "uyelik_durumu")
+                        query = "SELECT ad, soyad, cinsiyet, dogum_tarihi, kimlik_no, kan_grubu, uyelik_durumu, e_posta, sehir FROM uye where uyelik_durumu ='" + text + "'";
+                    else if (filter == "sehir")
+                        query = "SELECT ad, soyad, cinsiyet, dogum_tarihi, kimlik_no, kan_grubu, uyelik_durumu, e_posta, sehir FROM uye where sehir='" + text + "'";
+                    else if (filter == "kan_grubu")
+                        query = "SELECT ad, soyad, cinsiyet, dogum_tarihi, kimlik_no, kan_grubu, uyelik_durumu, e_posta, sehir FROM uye where kan_grubu ='" + text + "'";
+                    else
+                        query = "SELECT ad, soyad, cinsiyet, dogum_tarihi, kimlik_no, kan_grubu, uyelik_durumu, e_posta, sehir FROM uye";
+
+
                     using (OleDbCommand komut = new OleDbCommand(query, connection))
                     {
                         using (OleDbDataReader reader = komut.ExecuteReader())
@@ -41,7 +54,7 @@ namespace BusinessLayer
                                 membersArray[i, 0] = member.ad;
                                 membersArray[i, 1] = member.soyad;
                                 membersArray[i, 2] = member.cinsiyet;
-                                membersArray[i, 3] = member.dogum_tarihi.ToString(); // DateTime'i string'e dönüştür
+                                membersArray[i, 3] = member.dogum_tarihi.ToString();
                                 membersArray[i, 4] = member.kimlik_no;
                                 membersArray[i, 5] = member.kan_grubu;
                                 membersArray[i, 6] = member.uyelik_durumu;
@@ -57,8 +70,9 @@ namespace BusinessLayer
                 {
 
                 }
+
+
             }
         }
     }
 }
-
